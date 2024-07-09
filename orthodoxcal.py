2f19143cd50d61af9ef5b4864bb8b81f11b0d4e2
Mason <whitjm@outlook.com>
2024-07-08 21:19:12 -0400

    Removing local bible.txt and instead querying through https://bible-api.com/. Also updating with some cleaner formatting for printing the passages.

--- orthodoxcal.py
+++ orthodoxcal.py
@@ -1,31 +1,14 @@
 # OrthodoxCalendar.py
 # Grabs the Gregorian/Julian date, description, whether or not it's a fasting day, and the daily scripture readings. 
 # Mason Whitaker
-# 07/04/2024
+# Created: 07/04/2024
+# Updated: 07/08/2024
 
 import requests
 import os
 from requests_html import HTMLSession
-
-def find_passage(filename, book, chapter, start_verse, end_verse):
-    try:
-        with open(filename, 'r') as file:
-            found = False
-            result = []
-            for line in file:
-                columns = line.strip().split('\t')
-                if len(columns) >= 6:
-                    if columns[0] == book and columns[3] == chapter:
-                        if columns[4] == start_verse:
-                            found = True
-                        elif columns[4] == end_verse:
-                            result.append(columns[5])
-                            break
-                        elif found:
-                            result.append(columns[5])
-            return "\n".join(result) if result else "Text not found."
-    except FileNotFoundError:
-        return "File not found."
+import requests
+import textwrap
 
 url = "https://www.holytrinityorthodox.com/calendar/calendar.php"
 
@@ -48,17 +31,21 @@ print("\n")
 print(date.text,"\n")
 print(date_fasting.text,"\n")
 
-for citation in passages:
-    print(citation,"\n")
-    book = citation.split(' ', 1)[0]
-    chapterverse = citation.split(' ', 1)[1]
-    chapter = chapterverse.split(':')[0]
-    verse = chapterverse.split(':')[1]
-    versesplit_a = verse.split('-')[0]
-    versesplit_b = verse.split('-')[1]
-    filename = 'bible.txt'
-    result = find_passage(filename, book, chapter, versesplit_a,versesplit_b)
-    print(result,"\n")
+width = 120
 
-input("Press any key to exit...")
+for citation in passages:
+    book = citation.split(' ', 1)[0]
+    chapter = citation.split(' ',2)[1]
+    chapter = chapter.replace(',', '')
+    chapter = chapter.replace(';', '')
+
+    response = requests.get(str('https://bible-api.com/' + str(book) + str(chapter) +'?translation=kjv'))
+    data = response.json()
+    response_reference = data['reference']
+    response_text = textwrap.fill(data['text'], width=width)
+    response_text = response_text.replace('           ', '')
+    print(response_reference,"\n\n\t",response_text,"\n\n")
+    
+
+input("Press enter to exit...")
 
